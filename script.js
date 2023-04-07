74375c27bea4c58adeee249596e0e8e303b8a8ff
abhi20-1996 <110686880+abhi20-1996@users.noreply.github.com>
2023-04-07 09:56:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 // complete the given function
 
 function palindrome(str){
-
+const alphanumericOnly = str.toLowerCase().match(/[a-z0-9]/g);
+   if(alphanumericOnly.join('') ===
+        alphanumericOnly.reverse().join('')){
+  return true;
+        }
+        else{
+          return false;
+        }
 }
 module.exports = palindrome
